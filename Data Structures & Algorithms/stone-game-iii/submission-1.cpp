3d90949cffc9c4beeb436f6f16fb68b32b05d69e
AadityaSharma1001 <167458684+AadityaSharma1001@users.noreply.github.com>
2026-08-10 19:27:20 +0530

    Add: stone-game-iii - submission-1

---
+++ Data Structures & Algorithms/stone-game-iii/submission-1.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+    long long solve(int index, vector<int>& stoneValue, vector<long long>& dp) {
+        int n = stoneValue.size();
+        if (index >= n) return 0;
+        long long sum = 0;
+        long long ans = LLONG_MIN;
+
+        if(dp[index] != -1) return dp[index];
+
+        for (int i = index; i < min(n, index + 3); i++) {
+            sum += stoneValue[i];
+
+            ans = max(ans, sum - solve(i + 1, stoneValue, dp));
+        }
+
+        return dp[index] = ans;
+    }
+
+    string stoneGameIII(vector<int>& stoneValue) {
+        int n = stoneValue.size();
+
+        // vector<long long> dp(n+1, -1);
+
+        // long long ans = solve(0, stoneValue, dp);
+
+        // if (ans > 0) {
+        //     return "Alice";
+        // } else if (ans == 0) {
+        //     return "Tie";
+        // } else {
+        //     return "Bob";
+        // }
+
+        vector<int> dp(n+1, 0);
+        dp[n] = 0;
+
+        for(int i = n-1 ; i >= 0 ; i--) {
+            int sum = 0;
+            int ans = INT_MIN;
+            for(int j = i ; j < min(n, i + 3) ; j++) {
+                sum += stoneValue[j];
+
+                ans = max(ans, sum - dp[j + 1]);
+            }
+
+            dp[i] = ans;
+        }
+
+        if(dp[0] > 0) {
+            return "Alice";
+        } else if(dp[0] == 0) {
+            return "Tie";
+        } else {
+            return "Bob";
+        }
+    }
+};
